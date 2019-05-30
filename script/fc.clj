7db1af86ac788474550de6e5a73d3a2b36ac7c63
hanfei <lim@s-labs.cn>
2019-05-30 16:32:48 +0800

    support buy

--- script/fc.clj
+++ script/fc.clj
@@ -8,18 +8,23 @@
 ;; 间隔时间，毫秒
 (def interval (atom 9000))
 
-(def sell-amount (atom 1.0))    ;; 每笔卖单的数量
-
-(def sell-levels [1.028, 1.035, 1.05, 1.065, 1.08, 1.095, 1.11, 1.125, 1.14, 1.155])
-(def min-sell-level 1.025)
-(def max-sell-level 1.16)
-
 (def eoseth "eoseth")
+(def eosusdt "eosusdt")
+
+(def sell-amount (atom 1.0))    ;; 每笔卖单的数量
+(def sell-levels [1.023, 1.028, 1.033, 1.038, 1.05, 1.065, 1.08, 1.095, 1.11, 1.125])
+(def min-sell-level 1.02)
+(def max-sell-level 1.13)
+
+(def buy-amount (atom 1.0))    ;; 每笔买单的数量
+(def buy-levels [0.97, 0.955, 0.94, 0.925, 0.91, 0.895, 0.88, 0.865, 0.85])
+(def min-buy-level 0.845)
+(def max-buy-level 0.975)
 
 (defn init []
   (reset! prec (get {"eosusdt" 3, "eoseth" 5, "eosbtc" 7} @symbol))
-  (printf "symbom = {}, prec = {}, sell-amount = {}, interval = {}" 
-     @symbol @prec @sell-amount @interval)
+  (printf "symbom = {}, prec = {}, sell-amount = {}, buy-amount = {}, interval = {}" 
+     @symbol @prec @sell-amount @buy-amount @interval)
   (set-api @api-key @api-secret @symbol)
   (sleep 8000))
 
@@ -30,22 +35,12 @@
 (defn- compute-price [price factor]
     (ceiling (* price factor) @prec))
 
-(defn cancel-all []
-  (when *sell-orders
-      (printf "cancel {} sell orders" (count *sell-orders))
-      (map (cancel) *sell-orders)))
-
 (defn submit-sell [amount factor]
     (let [sell-price (compute-price *price-sell1 factor)]
         (sell @symbol sell-price amount)
         (printf "submit sell amount = {}, price = {}" amount, sell-price)))
 
-(defn submit-all []
-  (get-market @symbol)
-  (map (submit-sell @sell-amount) sell-levels)
-  (printf "submit ok"))
-
-(defn- cancel-submit [order sell-price]
+(defn- cancel-submit-sell [order sell-price]
     (cancel order)
     (sleep 500)   ;; 有时候取消订单没有及时完成，造成下面的挂单失败，所以暂停一会
     (sell @symbol sell-price @sell-amount)
@@ -55,7 +50,7 @@
     ; (printf "check sell order {} {} {}" price-min level-index order)
     (if (> price-min (str-to-decimal (:price order)))
       (let [sell-price (compute-price base-price (get sell-levels level-index))]
-        (cancel-submit order sell-price)
+        (cancel-submit-sell order sell-price)
         (- level-index 1))
       level-index))
 
@@ -63,7 +58,7 @@
     ; (printf "check max sell order {} {} {}" price-max level-index order)
     (if (< price-max (str-to-decimal (:price order)))
       (let [sell-price (compute-price base-price (get sell-levels level-index))]
-          (cancel-submit order sell-price)
+          (cancel-submit-sell order sell-price)
           (+ level-index 1))
       level-index))
 
@@ -79,12 +74,19 @@
           (printf "order changed, query again!")
           (get-orders @symbol))))
 
-(defn gua-dan []
+(defn sell-gua-dan []
     (init)
+
     (get-orders @symbol)
-    (cancel-all)
+    (when *sell-orders
+      (printf "cancel {} sell orders" (count *sell-orders))
+      (map (cancel) *sell-orders))
+
     (sleep 1000)
-    (submit-all)
+    (get-market @symbol)
+    (map (submit-sell @sell-amount) sell-levels)
+    (printf "submit all sell order ok")
+
     (sleep 5000)
     (get-orders @symbol)
     (while true 
@@ -92,13 +94,63 @@
         (get-market @symbol)
         (handle-sell-orders *price-sell1 *sell-orders)))
 
-;;  test code
-(def test-orders 
-  [{:id "1" :price "10.111" :amount "10.0"}
-   {:id "2" :price "10.312" :amount "10.0"}
-   {:id "3" :price "12.823" :amount "10.0"}
-   {:id "4" :price "10.613" :amount "10.0"}])
+;; buy functions
+(defn submit-buy [amount factor]
+    (let [buy-price (compute-price *price-buy1 factor)]
+        (buy @symbol buy-price amount)
+        (printf "submit buy amount = {}, price = {}" amount, buy-price)))
 
-(defn test []
-    (printf "test call")
-    (handle-sell-orders  10.0 test-orders))
+(defn- cancel-submit-buy [order buy-price]
+    (cancel order)
+    (sleep 500)   ;; 有时候取消订单没有及时完成，造成下面的挂单失败，所以暂停一会
+    (buy @symbol buy-price @buy-amount)
+    (printf "recreat buy order {} -> {}" (:price order) buy-price))
+
+(defn check-min-buy-order [base-price price-min level-index order]
+    ; (printf "check buy order {} {} {}" price-min level-index order)
+    (if (> price-min (str-to-decimal (:price order)))
+      (let [buy-price (compute-price base-price (get buy-levels level-index))]
+        (cancel-submit-buy order buy-price)
+        (+ level-index 1))
+      level-index))
+
+(defn check-max-buy-order [base-price price-max level-index order]
+    ; (printf "check max buy order {} {} {}" price-max level-index order)
+    (if (< price-max (str-to-decimal (:price order)))
+      (let [buy-price (compute-price base-price (get buy-levels level-index))]
+          (cancel-submit-buy order buy-price)
+          (- level-index 1))
+      level-index))
+
+; order process
+(defn handle-buy-orders [base-price orders]
+    ; (when debug (printf "handle buy orders {}" base-price))
+    (let [sorted-orders (sort [:price] orders)
+          price-min (compute-price base-price min-buy-level)
+          price-max (compute-price base-price max-buy-level)
+          ret1 (reduce (check-max-buy-order base-price price-max) (- (count buy-levels) 1) sorted-orders)
+          ret2 (reduce (check-min-buy-order base-price price-min) 0 sorted-orders)]
+        (when (or (not= ret1 (- (count buy-levels) 1)) (not= ret2 0))
+          (printf "order changed, query again!")
+          (get-orders @symbol))))
+
+(defn buy-gua-dan []
+    (init)
+
+    (get-orders @symbol)
+    (when *buy-orders
+      (printf "cancel {} buy orders" (count *buy-orders))
+      (map (cancel) *buy-orders))
+
+    (sleep 1000)
+    (get-market @symbol)
+    (map (submit-buy @buy-amount) buy-levels)
+    (printf "submit all buy order ok")
+
+    (sleep 5000)
+    (get-orders @symbol)
+    (while true 
+        (sleep @interval)
+        (get-market @symbol)
+        (handle-buy-orders *price-buy1 *buy-orders)))
+      
